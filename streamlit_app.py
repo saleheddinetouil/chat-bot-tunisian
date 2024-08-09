e1c1b3e01e4d339f739a473636c97691515c9fc0
Saleh eddine Touil <168127706+saleheddinetouil@users.noreply.github.com>
2024-08-09 21:28:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ model = genai.GenerativeModel(
 # Initialize chat history
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [
-        "U    ONLY  SPEAK    DARIJA   :    TUNISIAN ALGERIAN MAROCCAN"
+        "YOU ONLY SPEAK DARIJA : TUNISIAN ALGERIAN MAROCCAN, Use other languages only in explaining in french or english otherwise darija is main."
     ]
 
 # --- Streamlit App UI ---
